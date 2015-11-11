59bd3c90df96be5de547a2f48eecc1ca84f7cd9c
abekei0317 <abekei0317@gmail.com>
2015-11-11 12:40:42 +0000

    add valiadate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: {with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :about , length: { maximum: 255 } , presence: true, on: :update
+  validates :location , length: { maximum: 30 } , presence: true, on: :update
+
   has_secure_password
 end
